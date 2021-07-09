96e41e087c97fe26bed2f9f0341a3aeb45bd6b8a
ruancorreia <correia.ruan@gmail.com>
2021-07-08 22:01:46 -0300

    alterações no site

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-
+//importaçoes dos modulos
 const express = require('express');
-
 const app = express();
 
+//rotas
+app.use(express.static('public'));
+
+
 app.get('/', (req, res)=>{
-    res.send('Seja bem vindo!');
+    res.sendFile(__dirname + "/views/index.html")
+});
+
+app.get('/home', (req, res)=>{
+    res.sendFile(__dirname + "/views/index.html")
 });
 
 app.get('/manutencao', (req, res)=>{
     res.sendFile(__dirname + "/views/manutencao.html")
+});
+
+app.get('/blog', (req, res)=>{
+    res.sendFile(__dirname + "/views/blog.html")
 });
 
 app.listen(3000, ()=> console.log("Servidor iniciado!"));

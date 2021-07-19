d4442f85972ac3fa2e4259a6030c9c5eae219433
ruancorreia <correia.ruan@gmail.com>
2021-07-19 16:53:04 -0300

    atualizacoes no css

--- app.js
+++ app.js
@@ -7,20 +7,30 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + "/views/index.html")
+    res.sendFile(__dirname + "/views/home.html")
 });
 
 app.get('/home', (req, res)=>{
-    res.sendFile(__dirname + "/views/index.html")
+    res.sendFile(__dirname + "/views/home.html")
 });
 
 app.get('/manutencao', (req, res)=>{
     res.sendFile(__dirname + "/views/manutencao.html")
 });
 
+app.get('/produtos', (req, res)=>{
+    res.sendFile(__dirname + "/views/produtos.html")
+});
+
 app.get('/blog', (req, res)=>{
     res.sendFile(__dirname + "/views/blog.html")
 });
+
+app.get('/depoimento', (req, res)=>{
+    res.sendFile(__dirname + "/views/depoimento.html")
+});
+
+
 
 app.listen(3000, ()=> console.log("Servidor iniciado!"));
 

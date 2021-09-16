db6bda1bbdd4a60eb5903857dfeb9000b137f02b
ruancorreia <correia.ruan@gmail.com>
2021-09-16 12:41:39 -0300

    adicionado rotas

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const rotasDeProdutos = require("./routes/produtos")
 const rotas = require("./routes");
+const { urlencoded } = require('express');
 
 //rotas
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 app.use(express.static('public'));
 
 
@@ -36,6 +39,10 @@ app.get('/contato', (req, res)=>{
     res.sendFile(__dirname + "/views/contato.html")
 });
 
+app.post('/receber-contato', (req, res)=>{
+    console.log(req.body);
+    res.send("Olá " + req.body.nome + " recebemos a sua mensagem e entraremos em contato em breve!" )
+})
 
 app.listen(3000, ()=> console.log("Servidor iniciado!"));
 
